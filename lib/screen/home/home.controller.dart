67dbc791742020b54470add5a10bc7e511b040fb
Loc Tran <quangloc507@gmail.com>
2023-01-10 16:21:48 +0700

    [Feature] Fix page view

--- lib/screen/home/home.controller.dart
+++ lib/screen/home/home.controller.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
 import 'package:learn_with_mee/@core/data/repo/model/data_model.dart';
 import 'package:learn_with_mee/@core/data/repo/video_repo.dart';
@@ -11,13 +12,30 @@ class HomeController extends GetxController {
   final _thumbnailFile = "".obs;
   final videoPlayerController = VideoPlayerController.network("").obs;
   final isShowThumbnail = true.obs;
+  var initialVideo = true;
 
   Data? get dataVideo => _dataVideo.value;
 
   String? get thumbnailFile => _thumbnailFile.value;
 
+  PageController pageController = PageController(initialPage: 0, viewportFraction: 1);
+
   @override
   void onInit() {
+    pageController.addListener(() {
+      var page = pageController.page;
+      if(page != null) {
+        var pageRound = page.round();
+        if(page > page.toInt() + 0.5) {
+          final data = dataVideo?.data?[pageRound];
+          initVideoController(data?.urlVideoPlay ?? "");
+        }
+        else {
+          final data = dataVideo?.data?[pageRound];
+          initVideoController(data?.urlVideoPlay ?? "");
+        }
+      }
+    });
     super.onInit();
   }
 
